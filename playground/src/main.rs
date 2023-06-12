8d2edf9b5cf284aead9163fcba0e1dd1ae84f328
Andrei Matveiakin <a.matveiakin@gmail.com>
2023-06-12 15:52:48 +0400

    Empty playground

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,14 +1,5 @@
-use contest_lib_rs::io;
-
-#[allow(unused_variables)]
-fn solve<R: std::io::BufRead, W: std::io::Write>(read: &mut io::Reader<R>, write: &mut W) {
-    // ...
-}
-
 fn main() {
-    let mut read = io::Reader::new(std::io::stdin().lock());
-    let mut write = std::io::stdout().lock();
-    solve(&mut read, &mut write);
+
 }
 
 
@@ -19,4 +10,18 @@ fn main() {
 //     // ...
 //     let duration = start_time.elapsed();
 //     println!("Done in {duration:?}");
+// }
+
+
+// use contest_lib_rs::io;
+//
+// #[allow(unused_variables)]
+// fn solve<R: std::io::BufRead, W: std::io::Write>(read: &mut io::Reader<R>, write: &mut W) {
+//     // ...
+// }
+//
+// fn main() {
+//     let mut read = io::Reader::new(std::io::stdin().lock());
+//     let mut write = std::io::stdout().lock();
+//     solve(&mut read, &mut write);
 // }
