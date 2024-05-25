72f2d4dae2e9bdb5d741cb135bc6a36821208aea
Andrei Matveiakin <a.matveiakin@gmail.com>
2024-05-25 22:09:05 +0400

    Add helper to print Yes/No in title case

--- src/bool_ext.rs
+++ src/bool_ext.rs
@@ -1,8 +1,18 @@
+#[allow(non_snake_case)]
 pub trait BoolExtension {
+    fn YesNo(self) -> &'static str;
     fn yesno(self) -> &'static str;
 }
 
 impl BoolExtension for bool {
+    fn YesNo(self) -> &'static str {
+        if self {
+            "Yes"
+        } else {
+            "No"
+        }
+    }
+    // TODO: Rename to YESNO.
     fn yesno(self) -> &'static str {
         if self {
             "YES"
